04ac404f9c711076f58831d5add84784ba5a49e4
FabianaLLima <fabianallima@hotmail.com>
2024-07-26 15:47:31 -0300

    with '#' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   README.md
    	new file:   bin/App.class
    	new file:   bin/dominio/Banco.class
    	new file:   bin/dominio/Cliente.class
    	new file:   bin/dominio/ContaCorrente.class
    	new file:   bin/dominio/ContaPoupaca.class
    	new file:   src/App.java
    	new file:   src/dominio/Banco.java
    	new file:   src/dominio/Cliente.java

---
+++ src/dominio/ContaCorrente.java
@@ -0,0 +1,19 @@
+package dominio;
+
+public class ContaCorrente {
+
+    public int agencia; 
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+        
+    }
+}
---
+++ src/dominio/ContaPoupaca.java
@@ -0,0 +1,19 @@
+package dominio;
+
+public class ContaPoupaca {
+
+    public int agencia; 
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+        
+    }
+}
